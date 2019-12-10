bf000b9cd7dd4473e6229d2b7c7a528d1f03c53d
SamDing1998 <pd1341@nyu.edu>
2019-12-10 22:49:50 +0800

    Newest Version

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -35,4 +35,7 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import airline_staff
+    app.register_blueprint(airline_staff.bp)
+
     return app
