a73659bca242bcc0c68dad6e40bb30b563c0183d
SamDing1998 <pd1341@nyu.edu>
2019-12-11 15:50:21 +0800

    3 use case completed almost

--- flaskr/airline_staff.py
+++ flaskr/airline_staff.py
@@ -13,7 +13,6 @@ as_bp = Blueprint('airline_staff', __name__, url_prefix='/airline_staff')
 
 @as_bp.route('/view_my_flights', methods=('GET', 'POST'))
 def view_my_flights():
-    print(request.form)
     if request.method == "POST":
         from_date = request.form["from_date"]
         to_date = request.form["to_date"]
@@ -47,6 +46,54 @@ def view_my_flights():
 def home():
     return render_template('airline_staff/airline_staff.html')
 
+@as_bp.route('/view_booking_agents', methods=('GET', 'POST'))
+def view_booking_agents():
+    db = get_db()
+    from_date = str(datetime.now())
+    to_date_month = str(datetime.now() - relativedelta(days=30))
+    to_date_year = str(datetime.now() - relativedelta(year=1))
+    ticket_num_month = db.execute("SELECT booking_agent_id, COUNT(*) as count FROM purchases "
+                                  "where booking_agent_id is not null and purchase_date between ? and ?"
+                                  "GROUP BY booking_agent_id ORDER BY count DESC LIMIT 5", (to_date_month, from_date))
+    ticket_num_year = db.execute("SELECT booking_agent_id, COUNT(*) as count FROM purchases "
+                                 "where booking_agent_id is not null and  purchase_date between ? and ?"
+                                 "GROUP BY booking_agent_id ORDER BY count DESC LIMIT 5", (to_date_year, from_date))
+    commission = db.execute("SELECT booking_agent_id, SUM(price * 0.1) as total_commission FROM purchases JOIN ticket JOIN flight "
+                            "where booking_agent_id is not null and purchase_date between ? and ? and purchases.ticket_id == ticket.ticket_id and flight.flight_num == ticket.flight_num "
+                            "GROUP BY booking_agent_id ORDER BY total_commission DESC LIMIT 5", (to_date_year, from_date))
+    return render_template('airline_staff/view_booking_agents.html',
+                           ticket_num_month=ticket_num_month, ticket_num_year=ticket_num_year, commission=commission)
+@as_bp.route('/view_customers', methods=('GET', 'POST'))
+def view_customers():
+    airline_name = request.form['airline_name']
+    try:
+        cust_email = request.form['cust_email']
+    except:
+        cust_email = None
+    from_date = str(datetime.now())
+    to_date = str(datetime.now() - relativedelta(year=1))
+    db = get_db()
+    cust = db.execute("select email, name, count(ticket_id) as count1 from "
+                      "Customer natural join purchases natural join Ticket "
+                      "where airline_name=? and purchase_date between ? and ?"
+                      "group by email "
+                      "having count1 = "
+                      "(select max(c) from "
+                      "(select count(ticket_id) as c from "
+                      "Customer natural join purchases natural join Ticket "
+                      "where airline_name=? and purchase_date between ? and ?"
+                      "group by email))",
+                      (airline_name, to_date, from_date, airline_name, to_date, from_date))
+    if cust_email:
+        search = db.execute("select distinct Flight.flight_num, Flight.departure_time from "
+                            "Customer natural join purchases natural join Ticket inner join Flight "
+                            "on Ticket.flight_num=Flight.flight_num "
+                            "and Ticket.airline_name=Flight.airline_name "
+                            "where Flight.airline_name=? and email=?", (airline_name, cust_email))
+    else:
+        search = []
+
+    return render_template('view_cust.html', cust=cust, search=search)
 
 
 
